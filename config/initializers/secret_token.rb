4b08096bf8a887df37501b82f06b90e42449ba8b
beyondalbert <beyondalbert@gmail.com>
2014-01-09 20:12:55 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Applepie::Application.config.secret_key_base = 'c254b51c3f4d2617e8ebdf69e61d22bb6887e5c8ef94cf121ca1a3130c74c533c9d1c9c08dad5ba7a46a71335743b91a08162f614d6636fb9341648d73442ed7'
